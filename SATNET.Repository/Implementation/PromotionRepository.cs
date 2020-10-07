b2259bc13cfc7a0c173d92f29669b94477b6ea4b
metisumerkhalid <Umer.khalid@hypernymbiz.com>
2020-10-07 11:57:32 +0500

    prom changes

--- SATNET.Repository/Implementation/PromotionRepository.cs
+++ SATNET.Repository/Implementation/PromotionRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Configuration;
 using SATNET.Domain;
+using SATNET.Repository.Helper;
 using SATNET.Repository.Interface;
 using System;
 using System.Collections.Generic;
@@ -23,7 +24,17 @@ namespace SATNET.Repository.Implementation
         }
         public async Task<Promotion> Get(int id)
         {
-            throw new NotImplementedException();
+            var obj = new Promotion();
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                var parms = new DynamicParameters();
+                parms.Add("@Id", id, DbType.Int32, ParameterDirection.Input);
+                obj = await con.QueryFirstOrDefaultAsync<Promotion>("PromotionGet", parms, commandType: CommandType.StoredProcedure);
+            }
+            return obj;
         }
         public async Task<List<Promotion>> List(Promotion obj)
         {
@@ -40,7 +51,17 @@ namespace SATNET.Repository.Implementation
         }
         public async Task<int> Add(Promotion obj)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                var queryParameters = new DynamicParameters();
+                queryParameters.Add("@Id", obj.Id, DbType.Int32, ParameterDirection.InputOutput);
+                queryParameters.Add("@Name", obj.Name, DbType.String, ParameterDirection.Input);
+                queryParameters.Add("@LoginUserId", obj.CreatedBy, DbType.Int32, ParameterDirection.Input);
+                int retResult = await con.ExecuteScalarAsync<int>("PromotionAddOrUpdate", queryParameters, commandType: CommandType.StoredProcedure);
+                result = Parse.ToInt32(queryParameters.Get<int>("@Id"));
+            }
+            return result;
         }
         public async Task<int> Update(Promotion obj)
         {
@@ -48,7 +69,15 @@ namespace SATNET.Repository.Implementation
         }
         public async Task<int> Delete(int id, int deletedBy)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                var queryParameters = new DynamicParameters();
+                queryParameters.Add("@Id", id, DbType.Int32, ParameterDirection.Input);
+                queryParameters.Add("@LoginUserId", deletedBy, DbType.Int32, ParameterDirection.Input);
+                result = await con.ExecuteScalarAsync<int>("PromotionDelete", queryParameters, commandType: CommandType.StoredProcedure);
+            }
+            return result;
         }
     }
 }
--- SATNET.Service/Implementation/PromotionService.cs
+++ SATNET.Service/Implementation/PromotionService.cs
@@ -17,23 +17,115 @@ namespace SATNET.Service.Implementation
         }
         public async Task<Promotion> Get(int id)
         {
-            throw new NotImplementedException();
+            var retModel = new Promotion();
+            try
+            {
+                retModel = await _promotionRepository.Get(id);
+                if (retModel.Id != 0)
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+
+            }
+            return retModel;
         }
         public async Task<List<Promotion>> List(Promotion obj)
         {
             return await _promotionRepository.List(obj);
         }
-        public async Task<StatusModel> Add(Promotion Promotion)
+        public async Task<StatusModel> Add(Promotion obj)
         {
-            throw new NotImplementedException();
+            var status = new StatusModel { IsSuccess = false, ResponseUrl = "/Promotion/Index" };
+            try
+            {
+                int retId = -1;
+                retId = await _promotionRepository.Add(obj);
+                if (retId != 0)
+                {
+                    status.IsSuccess = true;
+                    status.ErrorCode = "Record inserted successfully.";
+                }
+                else
+                {
+                    status.IsSuccess = false;
+                    status.ErrorCode = "Error in inserting the record.";
+                }
+            }
+            catch (Exception e)
+            {
+                status.IsSuccess = false;
+                status.ErrorCode = "An error occured while processing request.";
+                status.ErrorDescription = e.Message;
+            }
+            finally
+            {
+            }
+            return status;
         }
-        public async Task<StatusModel> Update(Promotion Promotion)
+        public async Task<StatusModel> Update(Promotion obj)
         {
-            throw new NotImplementedException();
+            var status = new StatusModel { IsSuccess = false, ResponseUrl = "/Promotion/Index" };
+            try
+            {
+                int retId = -1;
+                retId = await _promotionRepository.Add(obj);
+                if (retId != 0)
+                {
+                    status.IsSuccess = true;
+                    status.ErrorCode = "Record updated successfully.";
+                }
+                else
+                {
+                    status.IsSuccess = false;
+                    status.ErrorCode = "Error in updating the record.";
+                }
+            }
+            catch (Exception e)
+            {
+                status.IsSuccess = false;
+                status.ErrorCode = "An error occured while processing request.";
+                status.ErrorDescription = e.Message;
+            }
+            finally
+            {
+            }
+            return status;
         }
         public async Task<StatusModel> Delete(int id, int deletedBy)
         {
-            throw new NotImplementedException();
+            var status = new StatusModel { IsSuccess = false, ResponseUrl = "/Promotion/Index" };
+            try
+            {
+                int retId = -1;
+                retId = await _promotionRepository.Delete(id, deletedBy);
+                if (retId >= 0)
+                {
+                    status.IsSuccess = true;
+                    status.ErrorCode = "Record deleted successfully.";
+                }
+                else
+                {
+                    status.IsSuccess = false;
+                    status.ErrorCode = "Error in deleting the record.";
+                }
+            }
+            catch (Exception e)
+            {
+                status.IsSuccess = false;
+                status.ErrorCode = "An error occured while processing request.";
+                status.ErrorDescription = e.Message;
+            }
+            finally
+            {
+            }
+            return status;
         }
 
     }
