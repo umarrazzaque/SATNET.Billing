45aec6f8ed4c7646855731f9863f123596fe4d35
umarrazzaque <umarrazzaque@hotmail.com>
2021-01-14 14:42:42 +0500

    update datatable call in Index

---
+++ SATNET.Domain/Enums/ImportExportDirectoryPath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SATNET.Domain.Enums
+{
+    public class ImportExportDirectoryPath
+    {
+        public const string ExcelImportPath = "Uploads/AirMAC";
+        public const string ExcelDownloadPath = "Mbps";
+    }
+}
